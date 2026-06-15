bd5e6e9236b43accce1cfc44980d28089a519015
Dingxingdi <784318034@qq.com>
2026-06-15 20:27:15 +0800

    Upload gen-arccos_ snapshot

---
+++ src/flag_gems/ops/arccos_.py
@@ -0,0 +1,27 @@
+import logging
+
+import triton
+import triton.language as tl
+
+from flag_gems.utils import pointwise_dynamic, tl_extra_shim
+
+_acos = tl_extra_shim.acos
+logger = logging.getLogger(__name__)
+
+
+@pointwise_dynamic(promotion_methods=[(0, "INT_TO_FLOAT")])
+@triton.jit()
+def arccos_kernel(x):
+    return _acos(x.to(tl.float32)).to(x.dtype)
+
+
+def arccos(x):
+    logger.debug("GEMS ARCCOS FORWARD")
+    y = arccos_kernel(x)
+    return y
+
+
+def arccos_(input):
+    logger.debug("GEMS ARCCOS_")
+    arccos_kernel(input, out0=input)
+    return input

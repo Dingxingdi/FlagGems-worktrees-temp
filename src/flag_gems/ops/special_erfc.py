82eca977c2f841c900dd85489c8a1467959271d8
Dingxingdi <784318034@qq.com>
2026-06-15 20:33:29 +0800

    Upload gen-special_erfc snapshot

---
+++ src/flag_gems/ops/special_erfc.py
@@ -0,0 +1,25 @@
+import logging
+
+import triton
+import triton.language as tl
+from flag_gems.utils import pointwise_dynamic
+
+logger = logging.getLogger(__name__)
+
+
+@pointwise_dynamic(promotion_methods=[(0, "DEFAULT")])
+@triton.jit
+def erfc_func(x):
+    # erfc(x) = 1 - erf(x)
+    output = 1.0 - tl.math.erf(x.to(tl.float32))
+    return output
+
+
+def erfc(x):
+    logger.debug("GEMS ERFC")
+    return erfc_func(x)
+
+
+def erfc_(x):
+    logger.debug("GEMS ERFC_")
+    return erfc_func(x, out0=x)

94ef7195439989fc03efc52784a2fb767faa633f
Dingxingdi <784318034@qq.com>
2026-06-15 20:31:32 +0800

    Upload gen-GreaterThan snapshot

---
+++ src/flag_gems/ops/GreaterThan.py
@@ -0,0 +1,36 @@
+# GreaterThan is the new name for gt in PyTorch 2.6+
+# It performs element-wise comparison: returns True where a > b
+
+import logging
+
+import triton
+import triton.language as tl
+
+from flag_gems.utils import pointwise_dynamic
+
+logger = logging.getLogger(__name__)
+
+
+@pointwise_dynamic(promotion_methods=[(0, 1, "ALWAYS_BOOL")])
+@triton.jit
+def greater_func(x, y):
+    return x.to(tl.float32) > y
+
+
+def greater(A, B):
+    logger.debug("GEMS GREATER")
+    return greater_func(A, B)
+
+
+@pointwise_dynamic(is_tensor=[True, False], promotion_methods=[(0, 1, "ALWAYS_BOOL")])
+@triton.jit
+def greater_func_scalar(x, y):
+    return x.to(tl.float32) > y
+
+
+def greater_scalar(A, B):
+    logger.debug("GEMS GREATER SCALAR")
+    return greater_func_scalar(A, B)
+
+
+__all__ = ["greater", "greater_scalar"]

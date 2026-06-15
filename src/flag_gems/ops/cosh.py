966a1f64e0cc8bedb78cf40e3efe5298d9761d96
Dingxingdi <784318034@qq.com>
2026-06-15 20:30:49 +0800

    Upload gen-cosh snapshot

---
+++ src/flag_gems/ops/cosh.py
@@ -0,0 +1,27 @@
+import logging
+
+import triton
+import triton.language as tl
+
+from flag_gems.utils import pointwise_dynamic
+
+logger = logging.getLogger(__name__)
+
+
+@pointwise_dynamic(promotion_methods=[(0, "INT_TO_FLOAT")])
+@triton.jit
+def cosh_func(x):
+    # cosh(x) = (e^x + e^(-x)) / 2
+    x_f32 = x.to(tl.float32)
+    return 0.5 * (tl.exp(x_f32) + tl.exp(-x_f32))
+
+
+def cosh(A):
+    logger.debug("GEMS COSH")
+    return cosh_func(A)
+
+
+def cosh_(A):
+    logger.debug("GEMS COSH_")
+    cosh_func(A, out0=A)
+    return A

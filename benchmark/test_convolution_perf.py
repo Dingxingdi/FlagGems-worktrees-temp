7c70362922acd4f6a6befc9dc8198c0cfbb53193
Dingxingdi <Dingxingdi@users.noreply.github.com>
2026-06-18 13:16:54 +0800

    Adapt benchmark TF32 config for GCU

--- benchmark/test_convolution_perf.py
+++ benchmark/test_convolution_perf.py
@@ -50,7 +50,8 @@ def test_perf_conv1d():
             "padding": padding,
         },
 
-    torch.backends.cudnn.allow_tf32 = False
+    if flag_gems.device == "cuda":
+        torch.backends.cudnn.allow_tf32 = False
     bench = Conv1DBenchmark(
         input_fn=conv1d_input_fn,
         op_name="conv1d",
@@ -110,7 +111,8 @@ def test_perf_conv2d():
 
     if flag_gems.vendor_name == "hygon":
         os.environ["TRITON_HIP_USE_NEW_STREAM_PIPELINE"] = "0"
-    torch.backends.cudnn.allow_tf32 = False
+    if flag_gems.device == "cuda":
+        torch.backends.cudnn.allow_tf32 = False
     bench = Conv2DBenchmark(
         input_fn=conv2d_input_fn,
         op_name="conv2d",
@@ -159,7 +161,8 @@ def test_perf_conv3d():
             "padding": padding,
         },
 
-    torch.backends.cudnn.allow_tf32 = False
+    if flag_gems.device == "cuda":
+        torch.backends.cudnn.allow_tf32 = False
     bench = Conv3DBenchmark(
         input_fn=conv3d_input_fn,
         op_name="conv3d",
@@ -219,7 +222,8 @@ def test_perf_cudnn_convolution():
             "allow_tf32": False,
         },
 
-    torch.backends.cudnn.allow_tf32 = False
+    if flag_gems.device == "cuda":
+        torch.backends.cudnn.allow_tf32 = False
     bench = CudnnConvBenchmark(
         input_fn=cudnn_conv_input_fn,
         op_name="cudnn_convolution",
